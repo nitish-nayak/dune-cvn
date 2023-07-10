4ba466289eaa5d146c5d5c638cee4c7f720e87ba
Nitish Nayak <felarof.nayak@gmail.com>
2023-07-10 16:06:53 -0400

    commit to utils

--- utils.py
+++ utils.py
@@ -6,6 +6,10 @@ __author__ = 'Saul Alonso-Monsalve, Leigh Howard Whitehead'
 __email__ = "saul.alonso.monsalve@cern.ch, leigh.howard.whitehead@cern.ch"
 
 import shutil
+
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
+
 import numpy as np
 import pickle as pk
 import sys
@@ -15,6 +19,7 @@ sys.path.append(os.path.join(sys.path[0], 'modules'))
 
 from tensorflow.keras.models import model_from_json
 from sklearn.metrics import classification_report, confusion_matrix
+from skimage.transform import resize
 from data_generator import DataGenerator
 from opts import get_args
 from keras.models import load_model
@@ -24,4 +29,3 @@ from dune_cvn import CustomTrainStep
 # manually specify the GPUs to use
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
-
